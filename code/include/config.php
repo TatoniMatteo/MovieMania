9cdb89b92a4719fa4a40218cc9869534614f0234
Matteo Tatoni <83955605+TatoniMatteo@users.noreply.github.com>
2023-06-24 13:07:00 +0200

    Template
    Caricamento della template
    Prime modifiche alla home

---
+++ code/include/config.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once "/dbm/dbm.php";
+require_once "/controller/filmController.php";
+require_once "/controller/serieController.php";
+
+class Config
+{
+    private static $instance;
+    private $database;
+
+    private function __construct()
+    {
+        $this->database = new Database();
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+
+    public function getFilmController()
+    {
+        return new FilmController($this->database);
+    }
+
+    public function getSerieController()
+    {
+        return new SerieController($this->database);
+    }
+
+    private function onClose()
+    {
+        $this->database->closeConnection();
+    }
+}
+
+/* 
+
+COME USARE?
+
+$filmController = Config::getFilmController();
+$serieController = Config::getSerieController();
+
+// Utilizza i controller per eseguire le operazioni desiderate
+$filmController->getAllFilms();
+$serieController->getAllSeries();
+*/
---
+++ code/include/controller/serieController.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "../dbm/dbm.php";
+
+class SerieController
+{
+    private $dbConnection;
+
+    public function __construct(Database $db)
+    {
+        $this->dbConnection = $db;
+    }
+
+    public function getAllSeries()
+    {
+        $query = "SELECT * FROM series";
+        $result = mysqli_query($this->dbConnection->getConnection(), $query);
+
+        // Gestisci il risultato della query come desideri
+        // ...
+    }
+
+    // Altri metodi per il controller delle serie...
+}
